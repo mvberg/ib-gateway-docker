6d5d23c2bb673bd37d57582df82b68498132f6cc
Douglas Spencer <forhire99@gmail.com>
2020-09-01 06:21:53 -0400

    Updates to Hashicorp Packer install to reduce image size

--- packer-install.sh
+++ packer-install.sh
@@ -3,29 +3,24 @@ export TZ=America/Chicago
 #COPY ./ib/IBController.ini /root/IBController/IBController.ini
 #COPY ./ib/jts.ini /root/Jts/jts.ini
 
+#export DISPLAY=:0
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata 
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y tzdata 
 
-  apt-get install -y wget \
-  && apt-get install -y unzip \
-  && apt-get install -y xvfb \
-  && apt-get install -y libxtst6 \
-  && apt-get install -y libxrender1 \
-  && apt-get install -y libxi6 \
-	&& apt-get install -y x11vnc \
-  && apt-get install -y socat \
-  && apt-get install -y software-properties-common \
+#apt-get install --no-install-recommends -y xubuntu-core^
+apt-get install --no-install-recommends -y  wget unzip xvfb socat ca-certificates x11vnc
+#libxtst6 libxrender1 libxi6 x11vnc socat 
+  #&& apt-get install -y software-properties-common \
 
 # Setup IB TWS
 mkdir -p /opt/TWS
 cd /opt/TWS
-wget -q https://download2.interactivebrokers.com/installers/ibgateway/stable-standalone/ibgateway-stable-standalone-linux-x64.sh
+wget https://download2.interactivebrokers.com/installers/ibgateway/stable-standalone/ibgateway-stable-standalone-linux-x64.sh
 chmod a+x ibgateway-stable-standalone-linux-x64.sh
 
 # Setup  IBController
 mkdir -p /opt/IBController/ && mkdir -p /opt/IBController/Logs
 cd /opt/IBController/
-ln -s scripts Scripts
 #wget -q http://cdn.quantconnect.com/interactive/IBController-QuantConnect-3.2.0.5.zip
 #unzip ./IBController-QuantConnect-3.2.0.5.zip
 wget https://github.com/IbcAlpha/IBC/releases/download/3.8.4-beta.1/IBCLinux-3.8.4-beta.1.zip
@@ -50,20 +45,16 @@ chmod -R u+x runscript.sh \
   && chmod 777 /etc/init.d/xvfb \
   && chmod 777 /etc/init.d/vnc
 
-echo "Showing root home contents" ; find /root
+#echo "Showing root home contents" ; find /root
 
 mkdir -p /root/IBController && cp /tmp/ib/IBController.ini /root/IBController/IBController.ini
 mkdir -p /root/jts && cp /tmp/ib/jts.ini /root/Jts/jts.ini
 
-#rm -fR /tmp/*
+rm -fR /tmp/*
 
-#dos2unix /usr/bin/xvfb-daemon-run \
-  #&& dos2unix /etc/init.d/xvfb \
-  #&& dos2unix /etc/init.d/vnc \
-  #&& dos2unix runscript.sh
+apt-get remove --purge -y distro-info-data dmsetup file gir1.2-glib-2.0 gir1.2-packagekitglib-1.0 glib-networking-common glib-networking-services gnupg-l10n gnupg-utils gpg gpg-agent gpg-wks-server gpgconf gpgsm iso-codes krb5-locales libapparmor1 libargon2-1 libasn1-8-heimdal libassuan0 libbrotli1 libcap2 libcap2-bin libcryptsetup12 libdconf1 libdevmapper1.02.1 libgirepository-1.0-1 libglib2.0-0 libglib2.0-bin libglib2.0-data libgssapi-krb5-2 libgssapi3-heimdal libgstreamer1.0-0 libhcrypto4-heimdal libheimbase1-heimdal libheimntlm0-heimdal libhx509-5-heimdal libicu66 libip4tc2 libjson-c4 libk5crypto3 libkeyutils1 libkmod2 libkrb5-26-heimdal libkrb5-3 libkrb5support0 libksba8 libldap-2.4-2 libldap-common liblmdb0 libmagic-mgc libmagic1 libmpdec2 libnpth0 libpackagekit-glib2-18 libpam-cap libpolkit-agent-1-0 libpolkit-gobject-1-0 libproxy1v5 libpython3-stdlib libpython3.8-minimal libpython3.8-stdlib libreadline8 libroken18-heimdal libsqlite3-0 libstemmer0d libwind0-heimdal libxml2 libyaml-0-2 lsb-release mime-support pinentry-curses python-apt-common python3 python3-apt python3-certifi python3-chardet python3-dbus python3-distro-info python3-gi python3-idna python3-minimal python3-pkg-resources python3-requests python3-requests-unixsocket python3-six python3-software-properties python3-urllib3 python3.8 python3.8-minimal readline-common shared-mime-info unattended-upgrades xdg-user-dirs xz-utils xfonts-base 
 
-# Below files copied during build to enable operation without volume mount
-#./ib/IBController.ini /root/IBController/IBController.ini
-#./ib/jts.ini /root/Jts/jts.ini
+apt-get -y autoclean 
+apt-get -y autoremove 
+rm -rf /var/lib/apt/lists/* /opt/IBController/IBCLinux-3.8.4-beta.1.zip /opt/TWS/ibgateway-stable-standalone-linux-x64.sh /opt/IBController/userguide.pdf
 
-#CMD bash runscript.sh
