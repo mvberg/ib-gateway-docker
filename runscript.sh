960953f5a46957589576c9b032692c81d0861b76
Douglas Spencer <forhire99@gmail.com>
2021-01-12 09:29:25 -0600

    Update socat in runscript

--- runscript.sh
+++ runscript.sh
@@ -11,4 +11,7 @@ while ! ss -ltn | grep 4001; do
 done
 
 echo "Forking :::4001 onto 0.0.0.0:4003\n"
-socat TCP-LISTEN:4003,fork TCP:127.0.0.1:4001
+#socat TCP-LISTEN:4003,fork TCP:127.0.0.1:4001
+
+socat -d -d -d  TCP-LISTEN:4003,forever,reuseaddr,keepalive,keepidle=10,keepintvl=10,keepcnt=2 TCP:127.0.0.1:4001
+
