8ebb6ba49a51930e86a1de6c91b4271d29ca24c4
Douglas Spencer <forhire99@gmail.com>
2021-02-04 10:54:19 -0500

    Update with SOCAT env vars

--- runscript.sh
+++ runscript.sh
@@ -3,6 +3,6 @@
 echo "Forking :::4001 onto 0.0.0.0:4003\n"
 #socat TCP-LISTEN:4003,fork TCP:127.0.0.1:4001
 
-socat -d -d -d  TCP-LISTEN:4003,fork,forever,reuseaddr,keepalive,keepidle=10,keepintvl=10,keepcnt=2 TCP:127.0.0.1:4001 &
+socat -d -d -d  TCP-LISTEN:${SOCAT_LISTEN_PORT},fork,forever,reuseaddr,keepalive,keepidle=10,keepintvl=10,keepcnt=2 TCP:${SOCAT_DEST_ADDR}:${SOCAT_DEST_PORT} &
 
 xvfb-daemon-run /opt/IBController/scripts/displaybannerandlaunch.sh 
