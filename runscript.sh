a6de4a600480f450907e81d0396a0c2880c2b0cb
Douglas Spencer <forhire99@gmail.com>
2021-02-04 10:05:26 -0500

    Update runscript to remove tail logs, since they can be sent to stdout with the path update now in the helm chart. Move to start socat first, IB Gateway start last so it should exit the pod if it crashes.

--- runscript.sh
+++ runscript.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-xvfb-daemon-run /opt/IBController/scripts/displaybannerandlaunch.sh &
-# Tail latest in log dir
-sleep 1
-tail -f $(find $LOG_PATH -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f 2-) &
-
-# Give enough time for a connection before trying to expose on 0.0.0.0:4003
-while ! ss -ltn | grep 4001; do
-   sleep 1
-done
-
 echo "Forking :::4001 onto 0.0.0.0:4003\n"
 #socat TCP-LISTEN:4003,fork TCP:127.0.0.1:4001
 
-socat -d -d -d  TCP-LISTEN:4003,fork,forever,reuseaddr,keepalive,keepidle=10,keepintvl=10,keepcnt=2 TCP:127.0.0.1:4001
+socat -d -d -d  TCP-LISTEN:4003,fork,forever,reuseaddr,keepalive,keepidle=10,keepintvl=10,keepcnt=2 TCP:127.0.0.1:4001 &
 
+xvfb-daemon-run /opt/IBController/scripts/displaybannerandlaunch.sh 
