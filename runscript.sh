6e26cfc2bd6673512bc7ff6c099637c1ff95bef8
Douglas Spencer <forhire99@gmail.com>
2023-03-16 10:54:57 -0400

    Updating startup scripts

--- runscript.sh
+++ runscript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+exec /usr/bin/tini --
 socat -d -d -d  TCP-LISTEN:${SOCAT_LISTEN_PORT},fork,forever,reuseaddr,keepalive,keepidle=10,keepintvl=10,keepcnt=2 TCP:${SOCAT_DEST_ADDR}:${SOCAT_DEST_PORT} &
 
 xvfb-daemon-run /opt/IBController/scripts/displaybannerandlaunch.sh 
